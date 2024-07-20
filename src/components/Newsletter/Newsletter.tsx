cd11cf8f9e850f9b0ad141628371294d03e8ee92
Per Eriksson <per@interapp.se>
2024-07-20 13:55:34 +0200

    wip

--- src/components/Newsletter/Newsletter.tsx
+++ src/components/Newsletter/Newsletter.tsx
@@ -2,15 +2,29 @@
 import {subscribeUserToList} from "@/apis/api";
 import {useRef, useState} from "react";
 
+const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
 export default function Newsletter() {
   const email = useRef<HTMLInputElement>(null)
-  const [subscribed, setSubscribed] = useState<boolean>(false)
+  const [msg, setMsg] = useState<string>("")
 
   async function subscribe(e: any) {
     e.preventDefault()
     if (!email.current || !email.current.value) return
+
+    if (!validateEmail(email.current.value)) {
+      setMsg("Please enter a valid email address.")
+      return
+    }
+
     await subscribeUserToList(email.current.value)
-    setSubscribed(true)
+    setMsg("You have now been subscribed!")
   }
 
   return (
@@ -21,8 +35,8 @@ export default function Newsletter() {
       <div className="middle">
         <h2>stay in the know</h2>
         <p>Enter your email address to receive news and updates from Padel&.</p>
-        {subscribed && (
-          <p>You have now been subscribed!</p>
+        {msg && (
+          <p>{msg}</p>
         )}
         <form>
           <input name="email" type="email" ref={email} placeholder="Email Address"/>

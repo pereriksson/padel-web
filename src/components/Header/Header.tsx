f0a5368b6bfadfaca0a5b44577a8381eaec5f050
Per Eriksson <per@interapp.se>
2024-07-24 18:54:18 +0200

    wip

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,66 @@
+"use client"
 import Image from "next/image";
 import Link from "next/link";
+import {useState} from "react";
+import classNames from "classnames";
+import OutsideClickHandler from 'react-outside-click-handler';
 
 export default function Header() {
+  const [showDrawer, setShowDrawer] = useState<boolean>(false)
+
+  const drawerClasses = classNames({
+    "header__drawer": true,
+    "visible": showDrawer
+  })
+
+  function toggleDrawer() {
+    setShowDrawer((prev) => !prev)
+  }
+
+  const links = [
+    ["/contact-us", "Contact Us"],
+    ["/membership", "Membership"],
+    ["/for-companies", "For Companies"],
+    ["/what-is-padel", "What is Padel"]
+  ]
+
   return (
     <div className="header">
       <div className="header__inner">
-        <div className="header__inner__logo">
-          <Link href="/">
-            <Image src="/images/logo.png" width="143" height="68" alt="Logo"/>
-          </Link>
-        </div>
-        <div className="header__links">
-          <Link href="/contact-us">Contact Us</Link>
-          <Link href="/membership">Membership</Link>
-          <Link href="/for-companies">For Companies</Link>
-          <Link href="/what-is-padel">What is Padel</Link>
-        </div>
-        <div className="header__inner__button">
-          <button>Book here</button>
+        <div className="links">
+          <div className="header__inner__logo">
+            <Link href="/">
+              <Image src="/images/logo.png" fill={true} alt="Logo"/>
+            </Link>
+          </div>
+          <div className="header__links">
+            {links.map((l, index) => (
+              <Link key={index} href={l[0]}>{l[1]}</Link>
+            ))}
+          </div>
+          <div className="header__inner__button">
+            <button>Book here</button>
+          </div>
+          <div>
+            <button className="header__inner__mobile-menu" onClick={toggleDrawer}>
+              <div className="segment"></div>
+              <div className="segment"></div>
+              <div className="segment"></div>
+            </button>
+          </div>
         </div>
       </div>
+      <OutsideClickHandler onOutsideClick={() => setShowDrawer(false)}>
+        <div className={drawerClasses}>
+          <menu>
+            {links.map((l, index) => (
+              <li>
+                <Link key={index} onClick={() => setShowDrawer(false)} href={l[0]}>{l[1]}</Link>
+              </li>
+            ))}
+          </menu>
+        </div>
+      </OutsideClickHandler>
     </div>
   )
 }

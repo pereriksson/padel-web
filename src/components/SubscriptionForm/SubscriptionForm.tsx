8d8c375b3a3a287267a13224e77616d2cb21b1d1
Per Eriksson <per@interapp.se>
2024-07-26 14:23:07 +0200

    wip

--- src/components/SubscriptionForm/SubscriptionForm.tsx
+++ src/components/SubscriptionForm/SubscriptionForm.tsx
@@ -1,5 +1,8 @@
+"use client"
 import Image from "next/image";
 import imageUrl from "@/utils/imageUrl";
+import {useState} from "react";
+import {sendEmail} from "@/apis/api";
 
 type SubscriptionFormProps = {
   cfd: any
@@ -7,9 +10,10 @@ type SubscriptionFormProps = {
 
 export default function SubscriptionForm(props: SubscriptionFormProps) {
   const {cfd} = props
+  const [msg, setMsg] = useState<string>("")
+
   const duration = ["1h", "1,5h", "2"]
   const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-
   const hours = []
   for (let i = 6; i <= 11; i++) {
     hours.push(`${i}am`)
@@ -18,8 +22,43 @@ export default function SubscriptionForm(props: SubscriptionFormProps) {
   for (let i = 1; i <= 10; i++) {
     hours.push(`${i}pm`)
   }
-
   const terms = ["6 Months", "12 Months"]
+
+  async function sendMsg(e: any) {
+    e.preventDefault()
+    const form = new FormData(e.currentTarget)
+    const firstName = form.get("firstName")
+    const lastName = form.get("lastName")
+    const email = form.get("email")
+    const phone = form.get("phone")
+    const duration = form.get("duration")
+    const day = form.get("day")
+    const startingTime = form.get("startingTime")
+    const term = form.get("term")
+    const message = form.get("message")
+
+    if (!email) {
+      return setMsg("Please enter an email address.")
+    }
+
+    await sendEmail(
+      cfd.fields.title,
+      `
+      First name: ${firstName}
+      Last name: ${lastName}
+      Email: ${email}
+      Phone: ${phone}
+      Duration: ${duration}
+      Day: ${day}
+      Starting time: ${startingTime}
+      Term: ${term}
+      Message:
+      ${message}
+      `
+    )
+
+    setMsg("Your message has been sent!")
+  }
 
   return (
     <div className="subscription-form">
@@ -29,27 +68,38 @@ export default function SubscriptionForm(props: SubscriptionFormProps) {
       <div className="text">
         <div className="inner">
           <h2>{cfd.fields.title}</h2>
-          <form>
-            <label>Duration</label>
+          {msg && (
+            <p>{msg}</p>
+          )}
+          <form onSubmit={sendMsg}>
+            <label>First name:</label>
+            <input type="text" name="firstName" autoComplete="given-name" placeholder="First Name"/>
+            <label>Last name:</label>
+            <input type="text" name="lastName" autoComplete="family-name" placeholder="Last Name"/>
+            <label>E-mail address:</label>
+            <input type="email" name="email" className="email" autoComplete="email" placeholder="Email"/>
+            <label>Phone number:</label>
+            <input type="tel" name="phone" className="phone" autoComplete="tel" placeholder="Phone Number"/>
+            <label>Duration:</label>
             <select name="duration">
               {duration.map((duration, index) => (
                 <option key={index} value={duration}>{duration}</option>
               ))}
             </select>
-            <label>Day</label>
+            <label>Day:</label>
             <select name="day">
               {days.map((day, index) => (
                 <option key={index} value={day}>{day}</option>
               ))}
             </select>
             <label>Starting time:</label>
-            <select name="starting-time">
+            <select name="startingTime">
               {hours.map((hour, index) => (
                 <option key={index} value={hour}>{hour}</option>
               ))}
             </select>
             <label>Term:</label>
-            <select name="starting-time">
+            <select name="term">
               {terms.map((term, index) => (
                 <option key={index} value={term}>{term}</option>
               ))}

f18c0dbbcc3ec728298c7ced6053a0c75b19265f
Per Eriksson <per@interapp.se>
2024-07-29 16:45:12 +0200

    wip

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link"
 
 export default function Footer() {
   return (
@@ -11,26 +12,25 @@ export default function Footer() {
           <div className="footer__inner__right">
             <div className="footer__inner__right__column">
               <h2>Visit us</h2>
-              <p>165 Eileen Way, Syosset, Long Island, New York, USA<br/>
-                Questions? Call<br/>
-                816-541-8270 (office)  or<br/>
-                855-752-7227</p>
+              <p>165 Eileen Way, Syosset,<br/>
+                Long Island, New York, USA<br/>
+                Questions? Email us at <Link target="_blank" href="mailto:hello@padeland.com">hello@padeland.com</Link><br/>
+                We’ll answer you within 24 hours</p>
             </div>
             <div className="footer__inner__right__column">
               <h2>OUR HOURS</h2>
-              <p>Restaurant open 10:45am - 3pm or sellout Tuesday’s thru Saturday.<br/>
-                No online orders or call-ins available</p>
+              <p>We are open from 6:00 AM to 11:30 PM. {/*All bookings must be made at <Link href="/bookhere">padeland.com/bookhere</Link>*/}</p>
             </div>
             <div className="footer__inner__right__column">
               <h2>KEEP IN TOUCH</h2>
-              <p>Follow us on Instagram<br/>
-                Like us on Facebook</p>
+              <p>Follow us on <Link target="_blank" href="https://www.instagram.com/padelandsyosset">Instagram</Link><br/>
+                Like us on <Link target="_blank" href="https://www.facebook.com/profile.php?id=61561065563307">Facebook</Link><br/>
+                Connect with us on <Link target="_blank" href="https://www.linkedin.com/company/padeland/">LinkedIn</Link></p>
             </div>
             <div className="footer__inner__right__column">
               <h2>FOR MEDIA INQUIRIES</h2>
-              <p>Deborah<br/>
-                orders@jonesbbqkc.com<br/>
-                Made with Squarespace</p>
+              <p>For media inquiries<br/>
+                Email us at <Link target="_blank" href="mailto:hello@padeland.com">hello@padeland.com</Link></p>
             </div>
           </div>
         </div>
